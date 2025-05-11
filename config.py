1792ae01d12066b66501b9bf6fa14a269cb78a07
Yeungbin Lee <yeungbinl@gmail.com>
2025-05-11 15:58:30 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+BASE_LIBRARY_PATH = "E:\\Downloads_NEW\\유성구 도서관"
+OUTPUT_TOP_BOOK_DIR = "data/processed"
+
+LIBRARY_FILE_NAMES = [
+    '관평도서관 장서 대출목록',
+    '구암도서관 장서 대출목록',
+    '구즉도서관 장서 대출목록',
+    '노은도서관 장서 대출목록',
+    '원신흥도서관 장서 대출목록',
+    '유성도서관 장서 대출목록',
+    '진잠도서관 장서 대출목록'
+]
+
+COLUMNS_TO_KEEP = ['도서명', '저자', '출판사', 'ISBN', '주제분류번호', '대출건수']
